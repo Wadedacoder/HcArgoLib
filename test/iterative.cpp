1c44459a954ff14369603e33ea4439361df6c0dc
Dev <dev21038@iiitd.ac.in>
2024-03-13 18:29:58 +0530

    Added iterative testing

---
+++ test/iterative.cpp
@@ -0,0 +1,89 @@
+#include<iostream>
+#include<algorithm>
+#include<stdio.h>
+#include<string.h>
+#include<cmath>
+#include<sys/time.h>
+
+#include "hclib.hpp"
+
+/*
+ * Ported from HJlib
+ *
+ * Author: Vivek Kumar
+ *
+ */
+
+//48 * 256 * 2048
+#define SIZE 25165824
+// #define SIZE 100
+#define ITERATIONS 64
+
+double* myNew, *myVal;
+int n;
+
+long get_usecs () {
+  struct timeval t;
+  gettimeofday(&t,NULL);
+  return t.tv_sec*1000000+t.tv_usec;
+}
+ 
+int ceilDiv(int d) {
+  int m = SIZE / d;
+  if (m * d == SIZE) {
+    return m;
+  } else {
+    return (m + 1);
+  }
+}
+
+void recurse(uint64_t low, uint64_t high) {
+  if((high - low) > 512) {
+    uint64_t mid = (high+low)/2;
+    // /* An async task */ recurse(low, mid);
+    hclib::finish([&]() {
+    hclib::async([&]( ){recurse(low, mid);});  
+    recurse(mid, high);
+    });
+;
+  } else {
+    // std::cout << "low: " << low << " high: " << high << std::endl;
+    for(uint64_t j=low; j<high; j++) {
+      myNew[j] = (myVal[j - 1] + myVal[j + 1]) / 2.0;
+    //   std::cout << "myNew[" << j << "]: " << myNew[j] << std::endl;
+    }
+  }
+}
+
+void runParallel() {
+  for(int i=0; i<ITERATIONS; i++) {
+    hclib::finish([&]() {
+    recurse(1, SIZE+1);
+    });
+    double* temp = myNew;
+    myNew = myVal;
+    myVal = temp;
+    
+  }
+}
+
+int main (int argc, char ** argv) {
+    hclib::init(argc, argv);
+    myNew = new double[(SIZE + 2)];
+    myVal = new double[(SIZE + 2)];
+    memset(myNew, 0, sizeof(double) * (SIZE + 2));
+    memset(myVal, 0, sizeof(double) * (SIZE + 2));
+    // for(int i=0; i<SIZE; i++) {
+    //   myVal[i] = i/5.0;
+    // }
+    myVal[5] = 10000.0;
+    std::cout << "Starting iterative.." << std::endl;
+    hclib::kernel([&]() {
+        runParallel();
+        });
+    std::cout << "Done!" << std::endl;
+    for(int i=0; i<10; i++) {
+      std::cout << myVal[i] << " ";
+    }
+    hclib::finalize();
+}
